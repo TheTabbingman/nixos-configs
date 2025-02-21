f0cae025a45281cc40cc1f9c9e2464487a1b7c83
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2025-02-21 12:29:13 -0600

    Move to one flake.nix with better layout.

--- flake.nix
+++ flake.nix
@@ -3,43 +3,60 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixos-wsl, ... }@inputs: let
+    inherit (self) outputs;
+
+    users = {
+      jonah = {
+        name = "jonah";
+      };
+    };
+
+    mkNixosConfiguration = username: hostname:
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs outputs hostname;
+          userConfig = users.${username};
+        };
+        modules = [./hosts/${hostname}/configuration.nix];
+      };
+
+    mkHomeConfiguration = username: hostname:
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs-unstable {system = "x86_64-linux";};
+        extraSpecialArgs = {
+          inherit inputs outputs;
+          userConfig = users.${username};
+          pkgs-stable = import nixpkgs {system = "x86_64-linux";};
+        };
+        modules = [
+          ./hosts/${hostname}/home-manager/home.nix
+        ];
+      };
+  in {
     nixosConfigurations = {
-      vm = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/vm/configuration.nix
-          # inputs.home-manager.nixosModules.default
-        ];
-      };
-      laptop = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/laptop/configuration.nix
-          # inputs.home-manager.nixosModules.default
-        ];
-      };
-      wsl = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        system = "x86_64-linux";
-        modules = [
-          nixos-wsl.nixosModules.default
-          ./hosts/wsl/configuration.nix
-          # inputs.home-manager.nixosModules.default
-        ];
-      };
+      vm = mkNixosConfiguration "jonah" "vm" ;
+      laptop = mkNixosConfiguration "jonah" "laptop";
+      wsl = mkNixosConfiguration "jonah" "wsl";
+    };
+
+    homeConfigurations = {
+      "jonah@nixos-vm" = mkHomeConfiguration "jonah" "vm";
+      "jonah@nixos-laptop" = mkHomeConfiguration "jonah" "laptop";
+      "jonah@wsl" = mkHomeConfiguration "jonah" "wsl";
     };
   };
 }

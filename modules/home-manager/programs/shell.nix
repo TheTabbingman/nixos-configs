4641979c3d64e50c52c2c4bb586e5b505c45442a
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2025-02-23 14:06:04 -0600

    Make shell.nix easier to read

--- modules/home-manager/programs/shell.nix
+++ modules/home-manager/programs/shell.nix
@@ -9,24 +9,24 @@
       pathFlakeLocation = "path:${flakeLocation}";
     in
     {
-      testflakeloc = "echo '${flakeLocation}'";
+      # edit
       he = "nvim ${flakeLocation}/hosts/${hostname}/home.nix";
-      hms = "home-manager switch --flake ${pathFlakeLocation} && chd";
-      hmu = "nix flake update --flake ${pathFlakeLocation}";
-      hmsu = "hmu && hms";
-      hd = "nvd diff $(home-manager generations | sed 's/.*-> //' | head -n 2 | tail -n 1) $(home-manager generations | sed 's/.*-> //' | head -n 1)";
       ne = "nvim ${flakeLocation}/hosts/${hostname}/configuration.nix";
       fe = "nvim ${flakeLocation}/flake.nix";
-      nrs = "sudo nixos-rebuild switch && cnd";
-      nrsu = "nu && nrs";
-      nrb = "sudo nixos-rebuild boot && cnbd";
-      nrbu = "nu && nrb";
-      nrt = "sudo nixos-rebuild test && ntd";
       nhe = "nvim ${flakeLocation}/hosts/${hostname}/configuration.nix ${flakeLocation}/hosts/${hostname}/home.nix";
+      # home-manager
+      hms = "home-manager switch --flake ${pathFlakeLocation} && chd";
+      # nix
+      nrs = "sudo nixos-rebuild switch && cnd";
+      nrb = "sudo nixos-rebuild boot && cnbd";
+      nrt = "sudo nixos-rebuild test && ntd";
       nu = "nix flake update --flake ${pathFlakeLocation}";
+      # diff
+      hd = "nvd diff $(home-manager generations | sed 's/.*-> //' | head -n 2 | tail -n 1) $(home-manager generations | sed 's/.*-> //' | head -n 1)";
       nd = "nvd diff $(ls -1d /nix/var/nix/profiles/system-* | sort -V | tail -n 2 | head -n 1) /run/current-system/";
       nbd = "nvd diff /run/current-system/ $(ls -1d /nix/var/nix/profiles/system-* | sort -V | tail -n 1)";
       ntd = "nvd diff $(ls -1d /nix/var/nix/profiles/system-* | sort -V | tail -n 1) /run/current-system/";
+      # other
       rm = "rm -I";
       fish-reload = "source ~/.config/fish/**/*.fish";
   };

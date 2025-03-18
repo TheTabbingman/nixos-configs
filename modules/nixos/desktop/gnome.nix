469c9da7018fbc506e76723a41eb0a8609c3e3d2
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2025-03-18 18:47:25 -0500

    Fix typo

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -5,5 +5,5 @@
 
   # Enable the Gnome Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 }

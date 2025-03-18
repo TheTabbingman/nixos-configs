2092a09532f3944556c0cc38f1add39243a9b3a0
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2025-03-18 18:40:20 -0500

    Add symlinking from dotfiles/.config to ~/.config

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,7 +1,19 @@
-{...}: {
+{config, ...}: let
+  configDirs = builtins.attrNames (builtins.readDir ../../dotfiles/.config);
+in {
   nix.gc = {
     automatic = true;
     frequency = "03:15";
     options = "--delete-older-than 14d";
   };
+
+  home.file = builtins.listToAttrs (
+    map (dir: {
+      name = ".config/${dir}";
+      value = {
+        source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/.config/${dir}";
+      };
+    })
+    configDirs
+  );
 }

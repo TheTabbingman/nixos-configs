54d974dbbdd4a6e380edb1668cd677729d45219f
TheTabbingman <51281790+TheTabbingman@users.noreply.github.com>
2025-03-26 11:42:16 -0500

    Switch laptop to gnome.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -11,7 +11,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    "${nixosModules}/desktop/kdeplasma.nix"
+    "${nixosModules}/desktop/gnome.nix"
     "${nixosModules}/system"
     "${nixosModules}/programs"
   ];

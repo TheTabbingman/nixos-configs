f65d7e1bfa8b8aee25deafc7cfe3fe6662103392
TheTabbingman <51281790+TheTabbingman@users.noreply.github.com>
2025-10-29 21:49:08 -0500

    Add keyring manager and gui

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -9,6 +9,13 @@
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
+
+  programs.regreet.enable = true;
+
+  # Keyring stuff
+  services.gnome.gnome-keyring.enable = true;
+  programs.seahorse.enable = true;
+
   # Optional, hint electron apps to use wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.systemPackages = with pkgs; [

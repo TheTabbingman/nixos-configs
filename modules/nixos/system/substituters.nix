88a3d968623dce6d332c0f20360b9322d9e0e42b
TheTabbingman <51281790+TheTabbingman@users.noreply.github.com>
2025-11-07 10:58:45 -0600

    Add walker cachix

--- modules/nixos/system/substituters.nix
+++ modules/nixos/system/substituters.nix
@@ -3,10 +3,13 @@
     substituters = [
       "https://hyprland.cachix.org"
       "https://cosmic.cachix.org"
+      # Might have to change to walker-git in the future
+      "https://walker.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
     ];
   };
 }

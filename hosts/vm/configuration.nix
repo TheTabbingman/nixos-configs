62aadd7bf89c9d7b6170849a17bf8e33d70effb0
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2025-11-29 16:54:49 -0600

    Switch vm to plasma

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -11,7 +11,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    "${nixosModules}/desktop/gnome.nix"
+    "${nixosModules}/desktop/plasma.nix"
     "${nixosModules}/system"
     "${nixosModules}/programs"
   ];

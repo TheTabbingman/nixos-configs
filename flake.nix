431af18adcc8849d8be6e893b1902aaaad2e0f8a
TheTabbingman <51281790+TheTabbingman@users.noreply.github.com>
2025-11-30 21:04:35 -0600

    Update nixpkgs-stable to 25.11
    I don't think I use this but should still update it

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";

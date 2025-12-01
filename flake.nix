f5d1515f459687743bbaa589576e3a6f2b566d16
TheTabbingman <51281790+TheTabbingman@users.noreply.github.com>
2025-11-30 20:56:56 -0600

    Revert "Update nixpkgs-stable to 25.11"
    This reverts commit c173696096fe521a511565fcefecd0e6f0137d45.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
 
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";

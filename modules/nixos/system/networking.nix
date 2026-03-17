a2e0d1c517319091c1a52b75cb397ccc5b643d9d
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-03-17 16:45:59 -0500

    Enable mounting fat-boy shares.

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -1,4 +1,30 @@
-{...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  # Define your share names here
+  shares = ["6X6" "3X4" "256GB NVME" "Ramdisk" "SSD" "Family"];
+
+  commonOptions = [
+    "x-systemd.automount"
+    "noauto"
+    "x-systemd.idle-timeout=60"
+    "x-systemd.device-timeout=5s"
+    "x-systemd.mount-timeout=5s"
+    "credentials=/home/jonah/smb-secrets"
+  ];
+in {
   # Enable networking
   networking.networkmanager.enable = true;
+
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [pkgs.cifs-utils pkgs.samba];
+
+  # This generates a fileSystems block for every share in the list
+  fileSystems = lib.genAttrs (map (share: "/mnt/share/fat-boy/${share}") shares) (mountPoint: {
+    device = "//fat-boy/${lib.last (lib.splitString "/" mountPoint)}";
+    fsType = "cifs";
+    options = commonOptions;
+  });
 }

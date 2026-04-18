91a84e184ed44e2d33f74aa8c256725e02ef2a2d
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-04-18 17:02:46 -0500

    Comment out unused inputs (can probably remove later)

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL/main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nixos-wsl = {
+    #   url = "github:nix-community/NixOS-WSL/main";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -20,15 +20,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "github:hyprwm/Hyprland";
-    split-monitor-workspaces = {
-      url = "github:Duckonaut/split-monitor-workspaces";
-      inputs.hyprland.follows = "hyprland";
-    };
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
+    # hyprland.url = "github:hyprwm/Hyprland";
+    # split-monitor-workspaces = {
+    #   url = "github:Duckonaut/split-monitor-workspaces";
+    #   inputs.hyprland.follows = "hyprland";
+    # };
+    # hyprland-plugins = {
+    #   url = "github:hyprwm/hyprland-plugins";
+    #   inputs.hyprland.follows = "hyprland";
+    # };
 
     stylix = {
       url = "github:nix-community/stylix";
@@ -47,7 +47,7 @@
 
     niri.url = "github:sodiboo/niri-flake";
 
-    ashell.url = "github:MalpenZibo/ashell";
+    # ashell.url = "github:MalpenZibo/ashell";
 
     dms = {
       url = "github:AvengeMedia/DankMaterialShell/stable";
@@ -70,7 +70,7 @@
     nixpkgs,
     nixpkgs-stable,
     home-manager,
-    nixos-wsl,
+    # nixos-wsl,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -127,24 +127,24 @@
         ];
       };
 
-    mkNixosWSLConfiguration = username: hostname:
-      nixpkgs.lib.nixosSystem {
-        specialArgs = let
-          pkgs-stable = import nixpkgs-stable {
-            system = "x86_64-linux";
-          };
-        in {
-          inherit inputs outputs pkgs-stable;
-          hostname = "nixos-" + hostname;
-          userConfig = users.${username};
-          nixosModules = "${self}/modules/nixos";
-        };
-        system = "x86_64-linux";
-        modules = [
-          nixos-wsl.nixosModules.default
-          ./hosts/${hostname}/configuration.nix
-        ];
-      };
+    # mkNixosWSLConfiguration = username: hostname:
+    #   nixpkgs.lib.nixosSystem {
+    #     specialArgs = let
+    #       pkgs-stable = import nixpkgs-stable {
+    #         system = "x86_64-linux";
+    #       };
+    #     in {
+    #       inherit inputs outputs pkgs-stable;
+    #       hostname = "nixos-" + hostname;
+    #       userConfig = users.${username};
+    #       nixosModules = "${self}/modules/nixos";
+    #     };
+    #     system = "x86_64-linux";
+    #     modules = [
+    #       nixos-wsl.nixosModules.default
+    #       ./hosts/${hostname}/configuration.nix
+    #     ];
+    #   };
 
     mkHomeConfiguration = username: hostname:
       home-manager.lib.homeManagerConfiguration {
@@ -170,14 +170,14 @@
       nixos-gamer = mkNixosConfiguration "jonah" "gamer";
       nixos-vm = mkNixosConfiguration "jonah" "vm";
       nixos-laptop = mkNixosConfiguration "jonah" "laptop";
-      nixos-wsl = mkNixosWSLConfiguration "jonah" "wsl";
+      # nixos-wsl = mkNixosWSLConfiguration "jonah" "wsl";
     };
 
     homeConfigurations = {
       "jonah@nixos-gamer" = mkHomeConfiguration "jonah" "gamer";
       "jonah@nixos-vm" = mkHomeConfiguration "jonah" "vm";
       "jonah@nixos-laptop" = mkHomeConfiguration "jonah" "laptop";
-      "jonah@nixos-wsl" = mkHomeConfiguration "jonah" "wsl";
+      # "jonah@nixos-wsl" = mkHomeConfiguration "jonah" "wsl";
     };
   };
 }

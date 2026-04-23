7c7d329cca5ca6665af42dfa34b0031207610895
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-04-23 09:53:05 -0500

    Setup vpn.

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -18,7 +18,12 @@
   ];
 in {
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
+  };
 
   # For mount.cifs, required unless domain name resolution is not needed.
   environment.systemPackages = [pkgs.cifs-utils pkgs.samba];
@@ -29,4 +34,30 @@ in {
     fsType = "cifs";
     options = commonOptions;
   });
+
+  # networking.wg-quick.interfaces = {
+  #   wg0 = {
+  #     autostart = true;
+  #     address = ["100.127.13.215/32"];
+  #     dns = ["10.255.255.3"];
+  #     privateKeyFile = "/home/jonah/wireguard-keys/private.key";
+  #     peers = [
+  #       {
+  #         publicKey = "5LYbbr320XMoXPrLsZex+2cDAMUOnzX5Htpcgb4Uc1c=";
+  #         presharedKeyFile = "/home/jonah/wireguard-keys/preshared.key";
+  #         allowedIPs = ["0.0.0.0/0"];
+  #         endpoint = "ord-323-wg.whiskergalaxy.com:443";
+  #         persistentKeepalive = 25;
+  #       }
+  #     ];
+  #   };
+  # };
+
+  services.openvpn.servers = {
+    windscribe = {
+      config = ''config /home/jonah/openvpn/Windscribe-Chicago-Wrigley.ovpn'';
+      authUserPass = "/home/jonah/openvpn/credentials";
+      autoStart = true;
+    };
+  };
 }

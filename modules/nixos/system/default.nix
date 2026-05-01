eab97af139a53e2966fd63be8bcaea6cd4e6aeb7
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-04-30 19:51:03 -0500

    Temp fix for openldap not building (required for bottles)

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -69,4 +69,14 @@
   };
 
   services.btrfs.autoScrub.enable = true;
+
+  # https://github.com/nixos/nixpkgs/issues/514113
+  # https://github.com/NixOS/nixpkgs/issues/513245
+  nixpkgs.overlays = [
+    (_: prev: {
+      openldap = prev.openldap.overrideAttrs {
+        doCheck = !prev.stdenv.hostPlatform.isi686;
+      };
+    })
+  ];
 }

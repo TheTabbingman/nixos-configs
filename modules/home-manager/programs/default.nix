89fdbbe583d5c79f336281598c671d665a74b42f
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-04-30 20:08:45 -0500

    Change back to anki-bin. The fix that I applied didn't seem to actually work. Wait for upstream fix.

--- modules/home-manager/programs/default.nix
+++ modules/home-manager/programs/default.nix
@@ -32,7 +32,7 @@
     fsearch
     inputs.json2nix.packages.${pkgs.stdenv.hostPlatform.system}.json2nix
     libreoffice-fresh
-    anki
+    anki-bin
     remmina
     chatterino2
     ungoogled-chromium

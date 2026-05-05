d22e447a1f01049d92c6e4481c9e87de6468b8a7
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-05-04 20:21:55 -0500

    Setup a minimal plasma install (haven't tested because I used hyprland for hdr instead)

---
+++ modules/nixos/desktop/plasma-minimal.nix
@@ -0,0 +1,29 @@
+{pkgs, ...}: {
+  services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    aurorae
+    plasma-browser-integration
+    plasma-workspace-wallpapers
+    konsole
+    kwin-x11
+    qttools # Expose qdbus in PATH
+    ark
+    elisa
+    gwenview
+    okular
+    kate
+    ktexteditor # provides elevated actions for kate
+    khelpcenter
+    dolphin
+    baloo-widgets # baloo information in Dolphin
+    dolphin-plugins
+    spectacle
+    ffmpegthumbs
+    krdp
+    kconfig # required for xdg-terminal from xdg-utils
+    qtbase # for qtpaths which is required for xdg-mime from xdg-utils
+    discover
+    qrca
+    kwallet
+  ];
+}

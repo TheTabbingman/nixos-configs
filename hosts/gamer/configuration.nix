22dc6f0c85beec66d6708effd326f57c35330797
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-05-07 13:33:05 -0500

    Move all firefox config to home-manager

--- hosts/gamer/configuration.nix
+++ hosts/gamer/configuration.nix
@@ -33,27 +33,6 @@
   # If used with Firefox
   environment.variables.MOZ_DISABLE_RDD_SANDBOX = "1";
 
-  programs.firefox = {
-    enable = true;
-    preferences = let
-      ffVersion = config.programs.firefox.package.version;
-    in {
-      "media.ffmpeg.vaapi.enabled" = lib.versionOlder ffVersion "137.0.0";
-      "media.hardware-video-decoding.force-enabled" = lib.versionAtLeast ffVersion "137.0.0";
-      "media.rdd-ffmpeg.enabled" = lib.versionOlder ffVersion "97.0.0";
-
-      "gfx.x11-egl.force-enabled" = true;
-      "widget.dmabuf.force-enabled" = true;
-
-      # Set this to true if your GPU supports AV1.
-      #
-      # This can be determined by reading the output of the
-      # `vainfo` command, after the driver is enabled with
-      # the environment variable.
-      "media.av1.enabled" = true;
-    };
-  };
-
   hardware.keyboard.qmk.enable = true;
   hardware.keyboard.qmk.keychronSupport = true;
 
--- modules/home-manager/programs/firefox/default.nix
+++ modules/home-manager/programs/firefox/default.nix
@@ -2,6 +2,7 @@
   pkgs,
   inputs,
   config,
+  lib,
   ...
 }: let
   extensionIds = [
@@ -97,7 +98,24 @@ in {
     configPath = "${config.xdg.configHome}/mozilla/firefox";
     profiles.default = {
       extensions.force = true;
-      settings = {
+      settings = let
+        ffVersion = config.programs.firefox.package.version;
+      in {
+        # For hw decoding
+        "media.ffmpeg.vaapi.enabled" = lib.versionOlder ffVersion "137.0.0";
+        "media.hardware-video-decoding.force-enabled" = lib.versionAtLeast ffVersion "137.0.0";
+        "media.rdd-ffmpeg.enabled" = lib.versionOlder ffVersion "97.0.0";
+
+        "gfx.x11-egl.force-enabled" = true;
+        "widget.dmabuf.force-enabled" = true;
+
+        # Set this to true if your GPU supports AV1.
+        #
+        # This can be determined by reading the output of the
+        # `vainfo` command, after the driver is enabled with
+        # the environment variable.
+        "media.av1.enabled" = true;
+
         "extensions.autoDisableScopes" = 0; # Don't disable extensions automatically
         /**
          **************************************************************************************

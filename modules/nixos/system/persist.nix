95bf6606d864d67a2b7378e347ef11a67b4f4f6e
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-05-08 16:35:36 -0500

    Cleanup and sort system persist

--- modules/nixos/system/persist.nix
+++ modules/nixos/system/persist.nix
@@ -1,34 +1,39 @@
-{lib, ...}: {
+{lib, ...}: let
+  mkPaths = base: paths: map (p: "${base}/${p}") paths;
+in {
   environment.persistence."/persist" = {
     enable = lib.mkDefault true; # NB: Defaults to true, not needed
     hideMounts = true;
     allowTrash = true;
-    directories = [
-      {
-        directory = "/etc/nixos";
-        user = "jonah";
-        group = "users";
-        mode = "u=rwx,g=r,o=";
-      }
-      "/etc/ssh"
-      "/var/log"
-      "/var/lib/bluetooth"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      "/var/db/sudo/lectured"
-      "/var/lib/tailscale"
-      "/var/lib/btrfs"
-      "/var/lib/upower"
-      "/var/lib/waydroid"
-      "/root/.cache/nix" # Needed so it doesn't reclone repos on every boot
-      {
-        directory = "/var/lib/colord";
-        user = "colord";
-        group = "colord";
-        mode = "u=rwx,g=rx,o=";
-      }
-    ];
+    directories =
+      [
+        {
+          directory = "/etc/nixos";
+          user = "jonah";
+          group = "users";
+          mode = "u=rwx,g=r,o=";
+        }
+        "/etc/NetworkManager/system-connections"
+        "/etc/ssh"
+        "/root/.cache/nix" # Needed so it doesn't reclone repos on every boot
+        "/var/db/sudo/lectured"
+        "/var/log"
+        {
+          directory = "/var/lib/colord";
+          user = "colord";
+          group = "colord";
+          mode = "u=rwx,g=rx,o=";
+        }
+      ]
+      ++ mkPaths "/var/lib" [
+        "bluetooth"
+        "btrfs"
+        "nixos"
+        "systemd/coredump"
+        "tailscale"
+        "upower"
+        "waydroid"
+      ];
     files = [
       "/etc/machine-id"
       "/var/lib/regreet/state.toml"

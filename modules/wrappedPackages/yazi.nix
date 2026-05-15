2c0a22e7aa8d3c4b980d63c9fbc34b7492d26ff6
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-05-15 16:13:24 -0500

    Uninstall bunny.yazi and switch to using cd.
    I don't know why I didnt' realize that I could just use cd. This is much
    simpler.

--- modules/wrappedPackages/yazi.nix
+++ modules/wrappedPackages/yazi.nix
@@ -56,25 +56,10 @@
           runHook postInstall
         '';
       };
-      bunny-yazi = pkgs.stdenv.mkDerivation {
-        name = "bunny.yazi";
-        src = pkgs.fetchFromGitHub {
-          owner = "stelcodes";
-          repo = "bunny.yazi";
-          rev = "71b14a3d624572f4884354c2e218296e9ece07cc";
-          sha256 = "sha256-uQO0C00yOFPWq8KEO/kEZM6tFZRc9SiXfgN7kzlwDeA=";
-        };
-        installPhase = ''
-          runHook preInstall
-          cp -r . $out
-          runHook postInstall
-        '';
-      };
     in {
       ffmpegthumbnailer = ffmpegthumbnailer-yazi;
       what-size = what-size-yazi;
       exifaudio = exifaudio-yazi;
-      bunny = bunny-yazi;
       compress = pkgs.yaziPlugins.compress;
       chmod = pkgs.yaziPlugins.chmod;
       lazygit = pkgs.yaziPlugins.lazygit;
@@ -142,9 +127,14 @@
             desc = "run lazygit";
           }
           {
-            on = ["'"];
-            run = "plugin bunny";
-            desc = "Start bunny.yazi";
+            on = ["g" "f"];
+            run = "cd /mnt/share/fat-boy";
+            desc = "Cd to fat-boy";
+          }
+          {
+            on = ["g" "n"];
+            run = "cd /etc/nixos";
+            desc = "Cd to nix config";
           }
         ];
       };
@@ -222,15 +212,6 @@
               require("git"):setup {
                   order = 1500,
               }
-
-              require("bunny"):setup({
-                  hops = {
-                      { key = {"f", "b"}, path = "/mnt/share/fat-boy/", desc = "fat-boy"},
-                      { key = {"f", "v", "6"}, path = "/mnt/share/fat-boy/6X6/Backups/Jonah's Stuff/Videos", desc = "fat-boy 6X6 videos"},
-                      { key = {"f", "v", "3"}, path = "/mnt/share/fat-boy/3X4/Backups/Jonah's Stuff/Videos", desc = "fat-boy 3X4 videos"},
-                      { key = "n", path = "/etc/nixos", desc = "NixOS Config"},
-                  },
-              })
           EOF
         '';
     };

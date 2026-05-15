a4312b2afae269c2fe9b5a1f24e27658e5228273
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-05-15 13:12:19 -0500

    Setup git signing

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -10,7 +10,11 @@
         };
         credential.helper = "cache --timeout 3600";
       };
-      signing.format = null;
+      signing = {
+        format = "ssh";
+        signByDefault = true;
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMeQLDYA1qvze3jRtokPl1q49UMKFNOyTFisntYe1siI 51281790+TheTabbingman@users.noreply.github.com";
+      };
     };
   };
 }

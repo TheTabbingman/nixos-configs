1ab63bfd6bf404fc0d28f59324a135abec1c65ab
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-05-19 13:05:02 -0500

    Move gaming flatpaks to gaming

--- modules/programs/flatpaks.nix
+++ modules/programs/flatpaks.nix
@@ -11,7 +11,7 @@
       ];
     };
   };
-  flake.homeModules.programs = {pkgs, ...}: {
+  flake.homeModules.programs = {...}: {
     imports = [
       inputs.nix-flatpak.homeManagerModules.nix-flatpak
     ];
@@ -19,16 +19,8 @@
       enable = true;
       uninstallUnmanaged = true;
       update.onActivation = true;
+      # Some packages are in gaming
       packages = [
-        rec {
-          appId = "org.flybywiresim.installer";
-          sha256 = "sha256-S68PYfVv5UqXf4yhwnHRth8MNMj43tQu9+6TitlMoac=";
-          bundle = "${pkgs.fetchurl {
-            url = "https://github.com/flybywiresim/installer/releases/download/v3.7.0/FlyByWire-Installer-3.7.0-x86_64.flatpak";
-            inherit sha256;
-          }}";
-        }
-        "xyz.rust4diva.Rust4Diva"
       ];
     };
   };
--- modules/programs/gaming/default.nix
+++ modules/programs/gaming/default.nix
@@ -35,5 +35,16 @@
       prismlauncher
       protonplus
     ];
+    services.flatpak.packages = [
+      rec {
+        appId = "org.flybywiresim.installer";
+        sha256 = "sha256-S68PYfVv5UqXf4yhwnHRth8MNMj43tQu9+6TitlMoac=";
+        bundle = "${pkgs.fetchurl {
+          url = "https://github.com/flybywiresim/installer/releases/download/v3.7.0/FlyByWire-Installer-3.7.0-x86_64.flatpak";
+          inherit sha256;
+        }}";
+      }
+      "xyz.rust4diva.Rust4Diva"
+    ];
   };
 }

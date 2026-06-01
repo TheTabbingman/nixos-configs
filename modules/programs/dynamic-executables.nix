18f3fb73c85ca1ade1a98f42a6da9df905797bdd
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-05-31 21:33:34 -0500

    Setup nix-alien

--- modules/programs/dynamic-executables.nix
+++ modules/programs/dynamic-executables.nix
@@ -1,33 +1,35 @@
 {inputs, ...}: {
-  flake.nixosModules.dynamic-executables = {pkgs, ...}: {
+  flake.nixosModules.system = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
-      nix-ld
       inputs.nix-alien.packages.${stdenv.hostPlatform.system}.nix-alien
     ];
-    # programs.nix-ld.libraries = with pkgs; [
-    #   libglvnd
-    #   libx11
-    #   libxrandr
-    #   libxxf86vm
-    #   libGL
-    #   libXcursor
-    #   libXi
-    #   libXinerama
-    #   libgcc
-    #   libxrender
-    #   libGL
-    #   libICE
-    #   libSM
-    #   libX11
-    #   libXext
-    #   libXfixes
-    #   libXi
-    #   libXrender
-    #   libgcc
-    #   libxkbcommon
-    #   cudaPackages.cudatoolkit
-    #   config.hardware.nvidia.package # Dynamically links your actual driver
+    # programs.nix-ld = {
+    # enable = true;
+    # libraries = with pkgs; [
+    # python3Packages.tkinter
+    # libglvnd
+    # libx11
+    # libxrandr
+    # libxxf86vm
+    # libGL
+    # libXcursor
+    # libXi
+    # libXinerama
+    # libgcc
+    # libxrender
+    # libGL
+    # libICE
+    # libSM
+    # libX11
+    # libXext
+    # libXfixes
+    # libXi
+    # libXrender
+    # libgcc
+    # libxkbcommon
+    # cudaPackages.cudatoolkit
+    # config.hardware.nvidia.package # Dynamically links your actual driver
     # ];
-    programs.nix-ld.enable = true;
+    # };
   };
 }

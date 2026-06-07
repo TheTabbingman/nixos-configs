8a79063aff48f46a14fe63887204c6d8a567dfe2
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-06-07 14:31:05 -0500

    Remove openldap overlay that seems to not be needed anymore

--- modules/system/overlays.nix
+++ modules/system/overlays.nix
@@ -8,11 +8,6 @@
       inputs.ulauncher.overlays.default
       # inputs.dolphin-overlay.overlays.default
       (final: prev: {
-        # https://github.com/nixos/nixpkgs/issues/514113
-        # https://github.com/NixOS/nixpkgs/issues/513245
-        openldap = prev.openldap.overrideAttrs {
-          doCheck = !prev.stdenv.hostPlatform.isi686;
-        };
         stable = import inputs.nixpkgs-stable {
           system = final.stdenv.hostPlatform.system;
           config = config.nixpkgs.config;

e71cd17105f482f129d8e25a1df90af96c01faff
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-06-23 13:58:28 -0500

    Setup samba share

--- hosts/gamer/configuration.nix
+++ hosts/gamer/configuration.nix
@@ -23,6 +23,7 @@
   flake.nixosModules.gamer = {
     pkgs,
     utils,
+    config,
     ...
   }: {
     networking.hostName = "nixos-gamer"; # Define your hostname.
@@ -44,6 +45,63 @@
     hardware.opentabletdriver.daemon.enable = true;
 
     boot.tmp.useTmpfs = true;
+
+    # Setup samba shares
+    services.samba = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        global = {
+          "workgroup" = "WORKGROUP";
+          "server string" = "smbnix";
+          "netbios name" = "smbnix";
+          "security" = "user";
+          #"use sendfile" = "yes";
+          #"max protocol" = "smb2";
+          # note: localhost is the ipv6 localhost ::1
+          "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+          "hosts deny" = "0.0.0.0/0";
+          "guest account" = "nobody";
+          "map to guest" = "bad user";
+        };
+        "Extra" = {
+          "path" = "/mnt/extra";
+          "browseable" = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force user" = "${config.preferences.user.name}";
+          "force group" = "users";
+          "valid users" = "${config.preferences.user.name}";
+        };
+      };
+    };
+    services.samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+    };
+    services.avahi = {
+      publish.enable = true;
+      publish.userServices = true;
+      # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+      nssmdns4 = true;
+      # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
+      enable = true;
+      openFirewall = true;
+    };
+    networking.firewall.enable = true;
+    networking.firewall.allowPing = true;
+    system.activationScripts = {
+      # The "init_smbpasswd" script name is arbitrary, but a useful label for tracking
+      # failed scripts in the build output. An absolute path to smbpasswd is necessary
+      # as it is not in $PATH in the activation script's environment. The password
+      # is repeated twice with newline characters as smbpasswd requires a password
+      # confirmation even in non-interactive mode where input is piped in through stdin.
+      init_smbpasswd.text = ''
+        /run/current-system/sw/bin/printf "$(/run/current-system/sw/bin/cat ${config.sops.secrets.jonah-password.path})\n$(/run/current-system/sw/bin/cat ${config.sops.secrets.jonah-password.path})\n" | /run/current-system/sw/bin/smbpasswd -sa ${config.preferences.user.name}
+      '';
+    };
 
     boot.initrd.systemd = {
       enable = true; # Default in 26.05

f21b39c159c403e5b5c2d94dc5d0254f4007671c
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2026-07-12 17:20:27 -0500

    Add trusted-users
    No idea if this really matters. I just saw it on the hyprland
    instructions for cachix.

--- modules/system/substituters.nix
+++ modules/system/substituters.nix
@@ -13,6 +13,7 @@
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
+      trusted-users = ["root" "@wheel"];
     };
   };
 }
